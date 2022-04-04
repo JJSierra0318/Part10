16ac06c8ad5746a76d907551e6d308883a9b52f4
JJSierra0318 <juanjosesierram@gmail.com>
2022-04-04 14:51:09 -0500

    feat: Added Sign in router

--- rate-repository-app/src/components/AppBar.jsx
+++ rate-repository-app/src/components/AppBar.jsx
@@ -1,6 +1,7 @@
-import { View, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable, ScrollView } from 'react-native';
 import Text from './Text'
 import Constants from 'expo-constants';
+import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -8,15 +9,21 @@ const styles = StyleSheet.create({
     height: 75,
     backgroundColor: '#24292e',
     marginBottom: 10,
+    flexDirection: 'row'
   },
   // ...
 });
 
 const AppBar = () => {
   return <View style={styles.container}>
-    <Pressable>
-      <Text color='primary'>Repositories</Text>
-    </Pressable>
+    <ScrollView horizontal>
+      <Pressable style={{padding: 10}}>
+        <Link to='/'><Text color='primary'>Repositories</Text></Link>
+      </Pressable>
+      <Pressable style={{padding: 10}}>
+      <Link to='/SignIn'><Text color='primary'>Sign In</Text></Link>
+      </Pressable>
+    </ScrollView>
   </View>;
 };
 
