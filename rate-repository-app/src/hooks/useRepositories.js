a54808d6e2687a15cdde63c3590d975f681f82b4
JJSierra0318 <juanjosesierram@gmail.com>
2022-04-11 13:34:00 -0500

    chore: Changed fetching repositories with Apollo Client

--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -1,17 +1,19 @@
 import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
+
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network'
+  })
+
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
 
   const fetchRepositories = async () => {
-    setLoading(true);
 
-    const response = await fetch('http://192.168.1.11:5000/api/repositories')
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
+    setRepositories(data.repositories);
   };
 
   useEffect(() => {
