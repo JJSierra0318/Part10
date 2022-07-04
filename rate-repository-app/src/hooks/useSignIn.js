e76f86cd5391397bc5a3ed2fbd797688ba8c083d
JJSierra0318 <juanjosesierram@gmail.com>
2022-07-04 15:34:02 -0500

    feat: Reset apolloClient store when user signs in

--- rate-repository-app/src/hooks/useSignIn.js
+++ rate-repository-app/src/hooks/useSignIn.js
@@ -1,19 +1,23 @@
 import { useMutation } from "@apollo/client";
 import { useNavigate } from "react-router-native";
+import { useApolloClient } from "@apollo/client";
 
 import { AUTHENTICATE } from "../graphql/mutations";
 import useAuthStorage from "./useAuthStorage";
 
 
 const useSignIn = () => {
-  let navigate = useNavigate()
+  const navigate = useNavigate();
   const authStorage = useAuthStorage();
+  const apolloClient = useApolloClient();
+
   const [mutate, result] = useMutation(AUTHENTICATE)
 
   const signIn = async ({username, password}) => {
 
     const response = await mutate( { variables: { username, password } })
     await authStorage.setAccessToken(response.data.authenticate.accessToken)
+    apolloClient.resetStore();
     navigate('/', {replace: true})
     return response
   }
